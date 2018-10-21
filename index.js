0c10cfa9306159c74e63d150397bcdb940f94ff9
Collin Bradley <herescollin@gmail.com>
2018-10-21 22:10:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var rankedListItems = document.querySelector("ul.ranked-list li");
+  for (var i = 0; i < rankedListItems.length; i++){
+    rankedListItems[i].innerHtml = rankedListItems[i].innerHtml.parseInt() + n;
+  }
+}
